5e509942e8c5abea129d4e22c12afb6cc81a7b36
saidjonjalolov <bmwmf8896@gmail.com>
2024-05-10 21:31:28 +0500

    PurchaseClothesImplimnt

--- Broker/Storeage/IStoreageBroker.cs
+++ Broker/Storeage/IStoreageBroker.cs
@@ -1,9 +1,5 @@
 ﻿using ClothesShop.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ClothesShop.Broker.Storeage
 {
@@ -15,7 +11,7 @@ namespace ClothesShop.Broker.Storeage
         Clothes UpdateClothes(int id, Clothes clothes);
         void AddClothes(Clothes clothes);
         void AddRangeClothes(List<Clothes> clothes);
-        void PurchaseClothes(string model);
-        List<Clothes> SoldInformationClothes();
+        void PurchaseClothes(string productType);
+        List<SoldProducts> SoldInformationClothes();
     }
 }
